4edc91e2324ed14a487a7e72222d6c39facb1035
HoseinSa <hoseinsadonasl@gmail.com>
2024-09-12 02:06:42 +0330

    Create app common components

---
+++ app/src/main/java/com/hotaku/minigallery/ui/components/TopAppBar.kt
@@ -0,0 +1,66 @@
+package com.hotaku.minigallery.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hotaku.minigallery.ui.theme.MiniGalleryTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MiniGalleryTopBar(
+    modifier: Modifier = Modifier,
+    title: String,
+    onNavClick: (() -> Unit)? = null,
+    actions: @Composable (RowScope.() -> Unit)? = null
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium
+            )
+        },
+        navigationIcon = onNavClick?.let {
+            {
+                MinimalIconButton(
+                    icon = Icons.AutoMirrored.Rounded.ArrowBack,
+                    onClick = onNavClick
+                )
+            }
+        } ?: {},
+        actions = actions ?: {}
+    )
+}
+
+@Preview
+@Composable
+private fun MiniGalleryTopBarPreview() {
+    MiniGalleryTheme(
+        darkTheme = true
+    ) {
+        MiniGalleryTopBar(
+            title = "Sample title",
+            onNavClick = {},
+            actions = {
+                MinimalIconButton(
+                    icon = Icons.Rounded.Star,
+                    onClick = {}
+                )
+                MinimalIconButton(
+                    icon = Icons.Rounded.Home,
+                    onClick = {}
+                )
+            }
+        )
+    }
+}

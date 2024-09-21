906f7558f15eea2c80561209f9bd37fcbb7c4a46
HoseinSa <hoseinsadonasl@gmail.com>
2024-09-21 20:50:29 +0330

    Add modules as dependencies to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,18 @@ android {
 
 dependencies {
 
+    implementation(project(":feature:home"))
+    implementation(project(":feature:albums"))
+    implementation(project(":libraries:core-ui"))
+    implementation(project(":libraries:navigation"))
+    implementation(project(":libraries:designsystem"))
+    implementation(project(":media:media-domain"))
+    implementation(project(":media:media-data"))
+    implementation(project(":core:database"))
+    implementation(project(":media:media-datasource"))
+    implementation(project(":core:datastore"))
+    implementation(project(":core:common"))
+
     implementation(libs.androidx.compose.material3.adaptive.layout)
     implementation(libs.androidx.compose.material3.adaptive.navigation)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)

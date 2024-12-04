99a265a953991de6b485817096f542a349bfb40b
HoseinSa <hoseinsadonasl@gmail.com>
2024-12-05 01:38:18 +0330

    Create core ui module

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,12 +1,16 @@
 plugins {
     alias(libs.plugins.minigallery.android.feature)
+    alias(libs.plugins.minigallery.android.hilt)
     alias(libs.plugins.minigallery.test.library)
     alias(libs.plugins.minigallery.test.android)
 }
 
 android {
-    namespace = "com.hotaku.libraries.core_ui"
+    namespace = "com.hotaku.core.ui"
 }
 
 dependencies {
+
+    implementation(libs.paging.compose)
+    implementation(libs.coil.compose)
 }

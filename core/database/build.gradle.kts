8f5afbabc60ea0cd683abc6a0e324b4e3ad2cef6
Hosein sa <hoseinsadonasl@gmail.com>
2025-01-06 20:49:17 +0330

    Remove unused UnitTest sourceset and unused files and gradle imports

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.minigallery.android.library)
     alias(libs.plugins.minigallery.android.hilt)
     alias(libs.plugins.minigallery.android.room)
-    alias(libs.plugins.minigallery.test.library)
     alias(libs.plugins.minigallery.test.android)
 }
 
--- media/media-datasource/src/main/kotlin/com/hotaku/datasource/entity/MediaEntity.kt
+++
@@ -1,6 +0,0 @@
-package com.hotaku.datasource.entity
-
-import androidx.room.Entity
-
-@Entity
-data class MediaEntity()

5ff1393786cd5decb09c76ee9b6af801c5a1c6c6
Hosein sa <hoseinsadonasl@gmail.com>
2025-01-14 23:19:32 +0330

    Refactor ErrorResult to be an interface instead of a class

--- core/domain/src/main/java/com/hotaku/domain/utils/ErrorResult.kt
+++ core/domain/src/main/java/com/hotaku/domain/utils/ErrorResult.kt
@@ -1,14 +1,14 @@
 package com.hotaku.domain.utils
 
-sealed class ErrorResult : Error {
-    data object UnknownError : ErrorResult()
+sealed interface ErrorResult : Error {
+    data object UnknownError : ErrorResult
 
     data class LocalError(
         val message: String,
-    ) : ErrorResult()
+    ) : ErrorResult
 
     data class ApiError(
         val code: Int = -1,
         val message: String,
-    ) : ErrorResult()
+    ) : ErrorResult
 }

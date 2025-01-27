109f0131915a2e9b60727b4fe56c98a906e07a11
Hosein sa <hoseinsadonasl@gmail.com>
2025-01-27 22:00:18 +0330

    Refactor MediaViewModel to synchronize and update media

--- feature/home/src/main/kotlin/com/hotaku/home/HomeScreenActions.kt
+++ feature/home/src/main/kotlin/com/hotaku/home/HomeScreenActions.kt
@@ -1,6 +1,8 @@
 package com.hotaku.home
 
 sealed interface HomeScreenActions {
+    data object UpdateMedia : HomeScreenActions
+
     data class OnQueryChange(val query: String) : HomeScreenActions
 
     data class OnMimeTypeChange(val mimeType: String) : HomeScreenActions
--- feature/home/src/main/kotlin/com/hotaku/home/MediaViewModel.kt
+++ feature/home/src/main/kotlin/com/hotaku/home/MediaViewModel.kt
@@ -3,6 +3,7 @@ package com.hotaku.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import androidx.paging.map
 import com.hotaku.domain.utils.DataResult
 import com.hotaku.domain.utils.Error
@@ -14,58 +15,78 @@ import com.hotaku.media_domain.usecase.GetMediaUseCase
 import com.hotaku.media_domain.usecase.SyncMediaUseCase
 import com.hotaku.ui.UiState
 import com.hotaku.ui.UiText
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 internal class MediaViewModel
     @Inject
     constructor(
         private val mapMediaToMediaUi: MapMediaToMediaUi,
-        syncMediaUseCase: SyncMediaUseCase,
-        mediaUseCase: GetMediaUseCase,
+        private val syncMediaUseCase: SyncMediaUseCase,
+        private val mediaUseCase: GetMediaUseCase,
     ) : ViewModel() {
         private var homeScreenViewModelState = MutableStateFlow(HomeScreenUiState())
-        val homeScreenUiState: StateFlow<HomeScreenUiState> =
-            homeScreenViewModelState.stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(5000),
-                initialValue = homeScreenViewModelState.value.copy(isLoading = true),
-            )
+        val homeScreenUiState: StateFlow<HomeScreenUiState> = homeScreenViewModelState
 
-        private var synchronizeViewModelState: StateFlow<UiState<Int>> =
-            syncMediaUseCase.invoke().map { result ->
-                when (result) {
-                    is DataResult.Loading -> UiState.Loading()
-                    is DataResult.Success -> UiState.Success(data = result.data ?: 0)
-                    is DataResult.Failure -> UiState.Failure(error = result.error.asUiError())
-                }
-            }.stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(5000),
-                initialValue = UiState.Loading(),
-            )
+        private var synchronizeViewModelState = MutableStateFlow<UiState<Int>>(UiState.Loading())
+        val synchronizeUiState =
+            synchronizeViewModelState
+                .onStart { synchronizeMedia() }
+                .stateIn(
+                    scope = viewModelScope,
+                    started = SharingStarted.WhileSubscribed(5000),
+                    initialValue = UiState.Loading(),
+                )
 
-        private var mediaViewModelState: StateFlow<PagingData<MediaUi>> =
-            mediaUseCase.invoke(
-                mimeType = homeScreenViewModelState.value.mimeType,
-                query = homeScreenViewModelState.value.query,
-            ).map { mediaList -> mediaList.map { media -> mapMediaToMediaUi.map(media) } }.stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(5000),
-                initialValue = PagingData.empty(),
-            )
-
-        val mediaUiState = mediaViewModelState
+        private var mediaViewModelState = MutableStateFlow<PagingData<MediaUi>>(PagingData.empty())
+        val mediaUiState =
+            mediaViewModelState
+                .onStart { updateMedia() }
+                .stateIn(
+                    scope = viewModelScope,
+                    started = SharingStarted.WhileSubscribed(5000),
+                    initialValue = PagingData.empty(),
+                )
 
         fun onAction(action: HomeScreenActions) {
             when (action) {
+                HomeScreenActions.UpdateMedia -> updateMedia()
                 is HomeScreenActions.OnMimeTypeChange -> setMimeType(action.mimeType)
                 is HomeScreenActions.OnQueryChange -> setQuery(action.query)
+            }
+        }
+
+        private fun synchronizeMedia() {
+            viewModelScope.launch {
+                syncMediaUseCase.invoke().collect { result ->
+                    synchronizeViewModelState.value =
+                        when (result) {
+                            is DataResult.Loading -> UiState.Loading()
+                            is DataResult.Success -> UiState.Success(data = result.data ?: 0)
+                            is DataResult.Failure -> UiState.Failure(error = result.error.asUiError())
+                        }
+                }
+            }
+        }
+
+        private fun updateMedia() {
+            viewModelScope.launch {
+                mediaUseCase.invoke(
+                    mimeType = homeScreenViewModelState.value.mimeType,
+                    query = homeScreenViewModelState.value.query,
+                )
+                    .cachedIn(viewModelScope)
+                    .collect { pagedMedia ->
+                        mediaViewModelState.value = pagedMedia.map { mapMediaToMediaUi.map(it) }
+                    }
             }
         }
 
